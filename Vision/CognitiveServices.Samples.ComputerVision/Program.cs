d5656a70693605d3babe56c24ec6a019bd24a064
Pratik Panda <mailtopratikpanda@gmail.com>
2019-05-01 11:17:13 +0530

    Modified thumbnail service.

--- Vision/CognitiveServices.Samples.ComputerVision/Program.cs
+++ Vision/CognitiveServices.Samples.ComputerVision/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CognitiveServices.Samples.ComputerVision.Helpers;
 using CognitiveServices.Samples.ComputerVision.Services;
 using System;
+using System.IO;
 
 namespace CognitiveServices.Samples.ComputerVision
 {
@@ -9,11 +10,19 @@ namespace CognitiveServices.Samples.ComputerVision
         public static void Main(string[] args)
         {
             string imageFilePath = @".\Assets\dirtCar.jpg";
-            // Analyze Image
             var imageData = FileHelper.GetContentAsByteArray(imageFilePath);
-            var service = new ImageAnalyzerService();
-            var analysis = service.AnalyzeImageAsync(imageData).Result;
-            service.WriteAnalysis(analysis);
+
+            //Analyze Image
+            //var service = new ImageAnalyzerService();
+            //var analysis = service.AnalyzeImageAsync(imageData).Result;
+            //service.WriteAnalysis(analysis);
+
+            //Generate Thumbnail
+            //var service = new ThumbnailGeneratorService();
+            //var thumbnailData = service.GenerateThumbnailAsync(imageData, 80, 50, true).Result;
+            //var thumbnailFilePath = string.Format("{0}\\thumbnail_{1:yyyy-MMM-dd_hh-mm-ss}.jpg",
+            //    Path.GetDirectoryName(imageFilePath), DateTime.Now);
+            //FileHelper.WriteByteArrayDataToFile(thumbnailData, thumbnailFilePath);
             Console.ReadKey();
         }
     }
